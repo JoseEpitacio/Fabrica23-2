2f081065f43c777b5506f328ac959edf2ab0eea1
JoseEpitacio <epitacioneto5@gmail.com>
2023-08-28 16:43:29 -0300

    Atividade 01 28/08

---
+++ dia1/atv01.py
@@ -0,0 +1,8 @@
+#1. crie um programa que receba um numero e mostre o antecessor e o sucessor
+
+numero = int(input("Digite um número: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f"O antecessor de {numero} é {antecessor}, e o sucessor é {sucessor}")
